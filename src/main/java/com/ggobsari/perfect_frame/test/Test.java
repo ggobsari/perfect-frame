41255750c1281fb49a121300e5acf8735c0bb368
ggobsari <ggobsari123@gmail.com>
2025-01-14 03:12:09 +0900

    타임리프를 통해 메인 레이아웃 전역 공유할 수 있도록 커스텀

--- src/main/java/com/ggobsari/perfect_frame/test/Test.java
+++ src/main/java/com/ggobsari/perfect_frame/test/Test.java
@@ -1,14 +1,21 @@
 package com.ggobsari.perfect_frame.test;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
+@Controller
 public class Test {
 
-    @GetMapping("/")
+    @GetMapping("/test")
     public String Main(){
-        return "index";
+        return "test";
+    }
+
+    @GetMapping("/layout")
+    public String Layout(){
+        return "layout/layout";
     }
 
 }
