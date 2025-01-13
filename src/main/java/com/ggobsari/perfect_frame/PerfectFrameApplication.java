1b651c6e3037304458640b90f1a92952291b57e9
ggobsari <ggobsari123@gmail.com>
2025-01-13 16:44:29 +0900

    perfect-frame 프로젝트 생성 및 깃허브 연동

---
+++ src/main/java/com/ggobsari/perfect_frame/PerfectFrameApplication.java
@@ -0,0 +1,13 @@
+package com.ggobsari.perfect_frame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfectFrameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerfectFrameApplication.class, args);
+	}
+
+}
